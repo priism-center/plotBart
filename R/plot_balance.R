9ceeb0cf5f7eacae0e312cdf818c792c7460839d
George Perrett <georgeperrett@10-17-91-87.dynapool.wireless.nyu.edu>
2022-10-11 14:39:00 -0400

    improved handeling of binary vars

--- R/plot_balance.R
+++ R/plot_balance.R
@@ -12,7 +12,7 @@
 #' @return ggplot object
 #' @export
 #'
-#' @import ggplot2 dplyr
+#' @import ggplot2 dplyr patchwork
 #' @importFrom tidyr pivot_longer pivot_wider
 #' @importFrom utils combn
 #' @importFrom stats reorder
@@ -22,8 +22,12 @@
 #' plot_balance(lalonde, 'treat', c('re78', 'age', 'educ'), compare = 'means', estimand = 'ATE') + labs(title = 'My new title')
 plot_balance <- function(.data, treatment, confounders, compare = c('means', 'variance', 'covariance'), estimand = c('ATE', 'ATT', 'ATC')){
   if(missing(treatment)) stop('enter a string indicating the name of the treatment variable')
+
   if (length(table(.data[[treatment]])) != 2) stop("treatment must be binary")
-  if(is.logical(.data[[treatment]])) .data[[treatment]] <- as.numeric(.data[[treatment]])
+
+  .data[[treatment]] <- coerce_to_logical_(.data[[treatment]])
+
+    # make sure arguments are set
   compare <- match.arg(compare)
   estimand <- match.arg(estimand)
   estimand <- toupper(estimand)
@@ -31,6 +35,7 @@ plot_balance <- function(.data, treatment, confounders, compare = c('means', 'va
   if (estimand %notin% c('ATE', 'ATT', 'ATC')) stop("estimand must be either: ATE, ATT or ATC")
   if(compare == 'variance')x_var <- 'variance' else x_var <- 'means'
 
+  # prep the df
   if(compare == 'covariance'){
     # gets interactions of all columns
     cov_dat <- combn(.data[, confounders], 2, FUN = Reduce, f = `*`)
@@ -45,9 +50,22 @@ plot_balance <- function(.data, treatment, confounders, compare = c('means', 'va
 
   }else{
     .data <- .data %>%
-      dplyr::select(all_of(c(confounders, treatment)))
+      dplyr::select(all_of(c(confounders, treatment))) %>%
+      rename(`treatment` = treatment)
+
   }
 
+  data_types <- apply(.data, 2, function(i) identical(unique(i)[order(unique(i))], c(0, 1))) %>%
+    as.data.frame()
+  data_types$name<- rownames(data_types)
+  names(data_types)[1] <- 'type'
+
+  data_types <- data_types %>%
+    mutate(type = if_else(type, 'binary/categorical', 'continuous')) %>%
+    arrange(name) %>%
+    filter(name != 'treatment')
+
+    # calculate and plot it
    .data <- .data %>%
     pivot_longer(cols = -treatment) %>%
     group_by(across(c('name', treatment))) %>%
@@ -55,60 +73,134 @@ plot_balance <- function(.data, treatment, confounders, compare = c('means', 'va
               variance = var(value),
               .groups = 'drop') %>%
     pivot_wider(names_from = treatment, values_from = c(variance, mean)) %>%
+    arrange(name)
+
+    .data <- cbind.data.frame(.data, type = data_types$type)
+
+   .data <- .data %>%
     mutate(means =
              case_when(
-               estimand == 'ATE' ~ (mean_1 - mean_0) / sqrt((variance_1 + variance_0) /2),
-               estimand == 'ATT' ~ (mean_1 - mean_0) / sqrt(variance_1),
-               estimand == 'ATC' ~ (mean_1 - mean_0) / sqrt(variance_0)
+               estimand == 'ATE' ~ (mean_TRUE - mean_FALSE) / sqrt((variance_TRUE + variance_FALSE) /2),
+               estimand == 'ATT' ~ (mean_TRUE - mean_FALSE) / sqrt(variance_TRUE),
+               estimand == 'ATC' ~ (mean_TRUE - mean_FALSE) / sqrt(variance_FALSE)
              ),
-           variance = sqrt(variance_1/variance_0)
-           ) %>%
-    mutate(flag_means = if_else(means > 2.5 | means < -2.5, 1, 0),
+           variance = sqrt(variance_TRUE/variance_FALSE),
+           means = if_else(type != 'continuous', mean_TRUE - mean_FALSE, means)
+           )
+
+   # calculate the order before rounding
+   if(compare == 'variance'){
+     .data <- .data %>%
+       mutate(order = if_else(variance < 1, 1/variance, variance))
+   }else{
+     .data <- .data %>%
+       mutate(order = abs(means))
+   }
+
+   .data <- .data %>%
+    mutate(flag_means = if_else(means > 2 | means < -2, 1, 0),
            flag_variance = if_else(variance > 4 | variance < .25, 1, 0),
            flag = if(compare == 'variance') flag_variance else  flag_means,
-           means = if_else(means > 2.5, 2.5, means),
-           means = if_else(means < -2.5, -2.5, means),
-           variance = if_else(variance > 1, 1, variance),
-           variance = if_else(variance < .5, .5, variance)) %>%
+           means = if_else(means > 2, 2, means),
+           means = if_else(means < -2, -2, means),
+           variance = if_else(variance > 4, 4, variance),
+           variance = if_else(variance < .25, .25, variance)) %>%
      na.omit()
 
-    p <- ggplot(.data, aes(
-      x = get(x_var),
-      y = reorder(name, abs(get(x_var))),
-      col = as.factor(flag)
-    )) +
-    geom_vline(
-      xintercept = ifelse(x_var == 'means', 0, 1),
-      linetype = 'dashed',
-      color = 'gray60'
-    ) +
-    geom_point(size = 4) +
-    scale_color_manual(values = c('black', 'red')) +
-    labs(
-      title = 'Balance',
-      x = case_when(
-        compare == 'means' ~ 'Scaled mean difference',
-        compare == 'variance' ~ 'Ratio of variance (log scale)',
-        compare == 'covariance' ~ 'Scaled mean difference of interactions (balance of covaraince)'
-      ),
-      y = NULL,
-      color = NULL
-    ) +
-    theme(legend.position = 'none')
 
-  if (x_var == 'means')
-    p <-p + coord_cartesian(xlim = c(-2.5, 2.5))
-    else
-      p <- p + coord_cartesian(xlim = c(.25, 4)) + scale_x_continuous(trans='log10')
-  if(max(.data$flag) == 0){
-    p <- p + labs(subtitle = 'points represent the treatment group')
+   p1 <- .data %>%
+     filter(type != 'continuous') %>%
+     ggplot(aes(
+       x = get(x_var),
+       y = reorder(name, order))) +
+     geom_vline(
+       xintercept = ifelse(x_var == 'means', 0, 1),
+       linetype = 'dashed',
+       color = 'gray60'
+     ) +
+     geom_point(size = 4) +
+     labs(
+       x = case_when(
+         compare == 'means' ~ 'Mean difference (Proportion)',
+         compare == 'variance' ~ 'Ratio of variance (log scale)',
+         compare == 'covariance' ~ 'Scaled mean difference of interactions (balance of covaraince)'
+       ),
+       y = NULL,
+       color = NULL
+     ) +
+     coord_cartesian(xlim = c(-1, 1)) +
+     theme(legend.position = 'none')
 
+   p2 <- .data %>%
+     filter(type == 'continuous') %>%
+     ggplot(aes(
+       x = get(x_var),
+       y = reorder(name, order),
+       col = as.factor(flag)
+     )) +
+     geom_vline(
+       xintercept = ifelse(x_var == 'means', 0, 1),
+       linetype = 'dashed',
+       color = 'gray60'
+     ) +
+     geom_point(size = 4) +
+     scale_color_manual(values = c('black', 'red')) +
+     labs(
+       x = case_when(
+         compare == 'means' ~ 'Standardized mean difference',
+         compare == 'variance' ~ 'Ratio of variance (log scale)',
+         compare == 'covariance' ~ 'Standardized mean difference of interactions (balance of covaraince)'
+       ),
+       y = NULL,
+       color = NULL
+     ) +
+     theme(legend.position = 'none')
+
+  if (compare == 'variance'){
+    p <- p2 +
+      coord_cartesian(xlim = c(.25, 4)) +
+      scale_x_continuous(trans='log10') +
+      labs(
+        title = 'Balance',
+        subtitle = if_else(
+          max(.data$flag) != 0,
+          'points represent the treatment group\nred points have a variance ratio that is more than 4 times bigger or smaller than the control group',
+          'points represent the treatment group'
+        )
+      )
   }else{
-      p <- p + labs(subtitle = if_else(compare == 'variance',
-                                       'points represent the treatment group\nred points have a variance ratio that is more than 2 times bigger or smaller than the control group',
-                                       'points represent the treatment group\nred points have standardized means that are more than 2.5 standard deviations different than the control group'))
+    p2 <-p2 + coord_cartesian(xlim = c(-2, 2))
+    if(length(unique(.data$type)) > 1){
+      p1 <- p1 + facet_wrap(~type)
+      p2 <- p2 + facet_wrap(~type)
+
+      p1 <- p1 + labs(
+        title = 'Balance',
+        subtitle = if_else(
+          max(.data$flag) != 0,
+          'points represent the treatment group\nred points have standardized means that are more than 2 standard deviations different than the control group',
+          'points represent the treatment group'
+        )
+      )
+
+      p <- p1 + p2
+
+    }else if(unique(.data$type == 'continuous')){
+      p <- p2 + labs(
+        title = 'Balance',
+        subtitle = if_else(
+          max(.data$flag) != 0,
+          'points represent the treatment group\nred points have standardized means that are more than 2 standard deviations different than the control group',
+          'points represent the treatment group'
+        )
+      )
+    }else{
+      p <- p1 + labs(
+        title = 'Balance',
+        subtitle =  'points represent the treatment group')
     }
 
+  }
 
   return(p)
 
@@ -137,14 +229,20 @@ plot_balance <- function(.data, treatment, confounders, compare = c('means', 'va
 print_balance <- function(.data, treatment, confounders, estimand = c('ATE', 'ATT', 'ATC')){
 
   if (length(table(.data[[treatment]])) != 2) stop("treatment must be binary")
-  if(is.logical(.data[[treatment]])) .data[[treatment]] <- as.numeric(.data[[treatment]])
+  .data[[treatment]] <- coerce_to_logical_(.data[[treatment]])
   estimand <- match.arg(estimand)
   estimand <- toupper(estimand)
 
   if (estimand %notin% c('ATE', 'ATT', 'ATC')) stop("estimand must be either: ATE, ATT or ATC")
 
-  classes <- sapply(.data[, confounders], class)
-  classes <- classes[order(classes)]
+  data_types <- apply(.data[, confounders], 2, function(i) identical(unique(i)[order(unique(i))], c(0, 1))) %>%
+    as.data.frame()
+  data_types$name<- rownames(data_types)
+  names(data_types)[1] <- 'type'
+
+  data_types <- data_types %>%
+    mutate(type = if_else(type, 'binary/categorical', 'continuous')) %>%
+    arrange(name)
 
   table <- .data %>%
     dplyr::select(all_of(c(confounders, treatment))) %>%
@@ -155,14 +253,14 @@ print_balance <- function(.data, treatment, confounders, estimand = c('ATE', 'AT
               .groups = 'drop') %>%
     pivot_wider(names_from = treatment, values_from = c(variance, mean)) %>%
     mutate(
-      raw_means = mean_1 - mean_0,
+      raw_means = mean_TRUE - mean_FALSE,
       means =
         case_when(
-          estimand == 'ATE' ~ (mean_1 - mean_0) / sqrt((variance_1 + variance_0) /2),
-          estimand == 'ATT' ~ (mean_1 - mean_0) / sqrt(variance_1),
-          estimand == 'ATC' ~ (mean_1 - mean_0) / sqrt(variance_0)
+          estimand == 'ATE' ~ (mean_TRUE - mean_FALSE) / sqrt((variance_TRUE + variance_FALSE) /2),
+          estimand == 'ATT' ~ (mean_TRUE - mean_FALSE) / sqrt(variance_TRUE),
+          estimand == 'ATC' ~ (mean_TRUE - mean_FALSE) / sqrt(variance_FALSE)
         ),
-           variance = sqrt(variance_1/variance_0)
+           variance = sqrt(variance_TRUE/variance_FALSE)
     ) %>%
     na.omit() %>%
     dplyr::select(name, raw_means, means, variance) %>%
@@ -175,8 +273,8 @@ print_balance <- function(.data, treatment, confounders, estimand = c('ATE', 'AT
 
     table$`standardized difference in means` <- as.character(table$`standardized difference in means`)
     table$`ratio of the variance` <- as.character(table$`ratio of the variance`)
-    table[classes != 'numeric', 3] <- '--'
-    table[classes != 'numeric', 4] <- '--'
+    table[data_types$type != 'continuous', 3] <- '--'
+    table[data_types$type != 'continuous', 4] <- '--'
 
     return(table)
 
@@ -184,7 +282,7 @@ print_balance <- function(.data, treatment, confounders, estimand = c('ATE', 'AT
 
 
 #' @title Print covariance statistics
-#' @description See balance statisitics of covariance for specidied variables between treatment and control groups.
+#' @description See balance statistics of covariance for specified variables between treatment and control groups.
 #'
 #' @param .data dataframe
 #' @param treatment the column denoted treatment. Must be binary.
@@ -207,7 +305,8 @@ print_covariance <- function(.data, treatment, confounders, estimand = c('ATE', 
 
   if(missing(treatment)) stop('enter a string indicating the name of the treatment variable')
   if (length(table(.data[[treatment]])) != 2) stop("treatment must be binary")
-  if(is.logical(.data[[treatment]])) .data[[treatment]] <- as.numeric(.data[[treatment]])
+  .data[[treatment]] <- coerce_to_logical_(.data[[treatment]])
+
   estimand <- match.arg(estimand)
   estimand <- toupper(estimand)
   classes <- sapply(.data[, confounders], class)
@@ -232,18 +331,18 @@ print_covariance <- function(.data, treatment, confounders, estimand = c('ATE', 
               .groups = 'drop') %>%
     pivot_wider(names_from = treatment, values_from = c(variance, mean)) %>%
     mutate(
-      raw_means = mean_1 - mean_0,
+      raw_means = mean_TRUE - mean_FALSE,
       means =
              case_when(
-               estimand == 'ATE' ~ (mean_1 - mean_0) / sqrt((variance_1 + variance_0) /2),
-               estimand == 'ATT' ~ (mean_1 - mean_0) / sqrt(variance_1),
-               estimand == 'ATC' ~ (mean_1 - mean_0) / sqrt(variance_0)
+               estimand == 'ATE' ~ (mean_TRUE - mean_FALSE) / sqrt((variance_TRUE + variance_FALSE) /2),
+               estimand == 'ATT' ~ (mean_TRUE - mean_FALSE) / sqrt(variance_TRUE),
+               estimand == 'ATC' ~ (mean_TRUE - mean_FALSE) / sqrt(variance_FALSE)
              ),
-      variance = sqrt(variance_1/variance_0)
+      variance = sqrt(variance_TRUE/variance_FALSE)
     ) %>%
     na.omit() %>%
     dplyr::select(name, means) %>%
-    arrange(desc(means)) %>%
+    arrange(desc(abs(means))) %>%
     rename(variable = name,`standardized difference in means` = means) %>%
     mutate(across(where(is.numeric), round, 2))
 
@@ -272,11 +371,11 @@ print_covariance <- function(.data, treatment, confounders, estimand = c('ATE', 
 
 plot_covariance <- function(.data, treatment, confounders){
   .data$treatment <- .data[[treatment]]
-  .data[, confounders] <- apply(.data[, confounders], 2, function(i) (i - mean(i))/sd(i))
+  #.data[, confounders] <- apply(.data[, confounders], 2, function(i) (i - mean(i))/sd(i))
   .data %>%
     GGally::ggpairs(
-      upper = list(continuous = "density"),
-      lower = list(continuous = "points"),
+      upper = list(continuous = "density", combo = "box_no_facet"),
+      lower = list(continuous = "points",  combo = "box_no_facet"),
       columns = confounders,
       aes(colour= as.factor(treatment), alpha = .7)) +
     scale_color_manual(values = c('blue', 'red')) +
